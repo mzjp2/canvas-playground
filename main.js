d7555a073e22d65d9829532a1e8cd08157f4b0b5
Zain Patel <30357972+mzjp2@users.noreply.github.com>
2018-02-06 22:02:01 +0000

    #updated

--- main.js
+++ main.js
@@ -1,34 +1,48 @@
-const canvas = document.querySelector('#draw');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+document.addEventListener('DOMContentLoaded', function() {
 
-ctx.strokeStyle = '#BADA55';
-ctx.lineWidth = 10;
+  const canvas = document.querySelector('#draw');
+  const body = document.querySelector('#body');
+  const ctx = canvas.getContext('2d');
+  canvas.width = body.offsetWidth;
+  canvas.height = body.offsetHeight
 
-var lastX = 0;
-var lastY = 0;
-var isDrawing = false;
+  document.querySelector('#button1').onclick = setColour;
 
-ctx.lineCap = 'round';
-ctx.lineJoin = 'round';
+  function setColour() {
+    var obj = document.querySelector('#mySelect');
+    var col = obj.options[obj.selectedIndex].text;
+    if (col == "red"){ctx.strokeStyle = '#FF0000'}
+    if (col == "blue"){ctx.strokeStyle = '#0000FF'}
+    //ctx.strokeStyle = '#BADA55';
+  }
+
+  ctx.lineWidth = 10;
+
+  var lastX = 0;
+  var lastY = 0;
+  var isDrawing = false;
+
+  ctx.lineCap = 'round';
+  ctx.lineJoin = 'round';
 
 
-function draw(e) {
-  if(!isDrawing) return;
-  ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
-  console.log(e);
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-}
+  function draw(e) {
+    if(!isDrawing) return;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    console.log(e);
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+  }
 
-addEventListener('mousedown', (e) => {
-  lastX = e.offsetX;
-  lastY = e.offsetY;
-  isDrawing = true;
-});
-addEventListener('mouseup', () => isDrawing = false);
-addEventListener('mouseout', () => isDrawing = false);
-addEventListener('mousemove', draw)
+  addEventListener('mousedown', (e) => {
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+    isDrawing = true;
+  });
+  addEventListener('mouseup', () => isDrawing = false);
+  addEventListener('mouseout', () => isDrawing = false);
+  addEventListener('mousemove', draw)
+
+})
